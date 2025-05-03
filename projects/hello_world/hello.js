73a83e7e407b90a43e32881e1e1539757492e2c7
Khumari Ayoub <mahskh08@gmail.com>
2025-05-03 16:00:54 +0430

    Install nodemon

--- projects/hello_world/hello.js
+++ projects/hello_world/hello.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world!\n");
+    res.send("What's up, Doc?\n");
 });
 
 app.listen(3000, "localhost", () => {
