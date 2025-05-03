5c18e73a7eabb638b3145a05372bbb9870388c4c
Khumari Ayoub <mahskh08@gmail.com>
2025-05-03 21:52:47 +0430

    Use Pug view engine

--- projects/hello_world/hello.js
+++ projects/hello_world/hello.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-    res.send("What's up, Doc?\n");
-});
+    res.render("hello-world-english")
+})
 
 app.listen(3000, "localhost", () => {
     console.log("Listening on port 3000.");
