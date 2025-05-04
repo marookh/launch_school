bc250453760a4a5163910997ff92f1e3aada85bf
Khumari Ayoub <mahskh08@gmail.com>
2025-05-04 16:02:50 +0430

    Add multiple languages

--- projects/hello_world/hello.js
+++ projects/hello_world/hello.js
@@ -4,9 +4,30 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static("public"))
 app.get("/", (req, res) => {
     res.render("hello-world-english")
 })
+
+app.get("/english", (req, res) => {
+  res.render("hello-world-english");
+});
+
+app.get("/french", (req, res) => {
+  res.render("hello-world-french");
+});
+
+app.get("/serbian", (req, res) => {
+  res.render("hello-world-serbian");
+});
+
+app.get("/pashto", (req, res) => {
+  res.render("hello-world-pashto");
+});
+
+app.get("/dari", (req, res) => {
+  res.render("hello-world-dari");
+});
 
 app.listen(3000, "localhost", () => {
     console.log("Listening on port 3000.");
